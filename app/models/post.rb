a81a1467ec468728c13b65b779800bf10fdd12d1
JulieGiles <julie_j_giles@hotmail.com>
2020-11-03 08:57:37 +1100

    Updated Comments controller and model and created a comments partial

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_one :attachment
   accepts_nested_attributes_for :attachment
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  # "dependent: :destroy" will delete associated comments if the post is destroyed
 end
