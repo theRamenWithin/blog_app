bb8fc8c0b51138ccf41145f974cd8320dd53a2fd
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2020-11-03 14:03:17 +1100

    Fixed signup, added bootstrap-icons

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
+
+  # validates :username,   presence: true, uniqueness: true
+  # validates :first_name, presence: true
+  # validates :last_name,  presence: true
+  # validates :DOB,        presence: true
+  # validates :email,      presence: true, uniqueness: true
+  
 end
