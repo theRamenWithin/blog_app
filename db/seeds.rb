526499df4aab5c49159fb99d2c7772eefc4dd797
JulieGiles <julie_j_giles@hotmail.com>
2020-11-04 09:31:05 +1100

    Generated fake data by updating seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,49 @@
 require 'pg'
 require 'faker'
 
-#Fake data for 20 records in User model
 
-(1..20).each do 
-    User.create!(
-        first_name: Faker::Name.name,
-        last_name: Faker::Name.name,
-        email: Faker::Internet.email,
-        DOB: Faker::Date.between(from: '1930-01-01', to: '2020-01-01'),
-        password: "password",
-        password_confirmation: "password",
-        username: Faker::String.name,
-        # admin?: Faker::Boolean.boolean,
-        post_id: Faker::Number.number(digits: 2)
-    )
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+Attachment.destroy_all
+
+
+#Fake data for 20 records in Users table
+20.times do
+    user = User.new
+    user.first_name = Faker::Name.first_name
+    user.last_name = Faker::Name.last_name
+    user.email = Faker::Internet.email
+    user.DOB = Faker::Date.between(from: '1930-01-01', to: '2020-01-01')
+    user.password = "password"
+    user.password_confirmation = "password"
+    user.username = Faker::String.name
+    user.save
+
+        # Fake data for Posts table
+        2.times do
+            post = Post.new
+            post.title = Faker::Games::LeagueOfLegends.quote
+            post.body = Faker::Lorem.sentence(word_count: 40)
+            post.likes = Faker::Number.number(digits: 2)
+            post.reaction = Faker::Number.number(digits: 2)
+            post.user_id = user.id
+            post.save
+
+                #Fake data for Comments table
+                3.times do
+                    comment = Comment.new
+                    comment.body = Faker::Lorem.sentence(word_count: 15)
+                    comment.post_id = post.id
+                    comment.user_id = user.id
+                    comment.save
+                end 
+
+                #Fake data for Attachments table (1 attachment per Post)
+                attachment = Attachment.new
+                attachment.file_name = Faker::Lorem.sentence(word_count: 3)
+                attachment.object = Faker::Lorem.sentence(word_count: 10)
+                attachment.post_id = post.id
+                attachment.save
+        end 
 end 
-
-#Fake data for Post model 
-# (1..100).each do 
-#     Post.create!(
-#         title: Faker::Hipster.words(number: 3), 
-#         body: Faker::Lorem.sentence(word_count: 40),
-#         likes: Faker::Number.number(digits: 2),
-#         reaction: Faker::Number.number(digits: 2)
-#     )
-# end 
-
-#Fake data for Attachment model
-# (1..5).each do 
-#     Attachment.create!(
-#         file_name: Faker::Lorem.sentence(word_count: 5),
-#         object: Faker::Lorem.sentence(word_count: 5)
-#     )
-# end 
-
-#Fake data for Comment model
-# (1..5).each do 
-#     Comment.create!(    
-#         body: Faker::Lorem.sentence(word_count: 40),
-#         likes: Faker::Number.number(digits: 2)
-#     )
-# end 
-
