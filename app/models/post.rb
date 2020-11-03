b3b6385b08976ba403b74e6ae4a934a0277c5920
jacksonchaffey <jacksonchaffey@gmail.com>
2020-11-03 15:30:29 +1100

    Fixed ability to post with refactoring of logic to attribute it to current user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one :attachment
-  has_many :comments, dependent: :destroy
-  
+  has_many :comments, dependent: :destroy 
   accepts_nested_attributes_for :attachment
   validates :title, :body, presence: true
 end
